bf1ef34817edb98920dd43b6f475a5bf29aeba25
Ben Hall <ben@benhall.me.uk>
2016-12-22 20:53:56 +0100

    Update Enable WinRM

--- enable-winrm.ps1
+++ enable-winrm.ps1
@@ -10,8 +10,5 @@ Set-NetConnectionProfile                                                       `
     -InterfaceIndex (Get-NetConnectionProfile).InterfaceIndex                  `
     -NetworkCategory Private
 
-Set-WSManQuickConfig -Force
 Set-Item WSMan:\localhost\Service\AllowUnencrypted -Value True
 Set-Item WSMan:\localhost\Service\Auth\Basic       -Value True
-Set-Service WinRM -StartupType Automatic
-Restart-Service winrm
